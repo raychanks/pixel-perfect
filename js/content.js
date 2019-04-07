2781cb99644f3e7c5ae69e6a932be078e0538c95
Ray Chan <football2000003@hotmail.com>
2019-04-07 23:26:54 +0800

    Add selection box UI

--- js/content.js
+++ js/content.js
@@ -1,3 +1,62 @@
+const coordinates = {
+    start: {
+        page: {
+            x: 0,
+            y: 0,
+        },
+        client: {
+            x: 0,
+            y: 0,
+        },
+    },
+    end: {
+        page: {
+            x: 0,
+            y: 0,
+        },
+        client: {
+            x: 0,
+            y: 0,
+        },
+    },
+};
+let isMouseDown = false;
+
+const eventHandlers = {
+    overlayMouseDown(event) {
+        isMouseDown = true;
+
+        coordinates.start.page.x = event.pageX;
+        coordinates.start.page.y = event.pageY;
+        coordinates.start.client.x = event.clientX;
+        coordinates.start.client.y = event.clientY;
+
+        renderSelectionBoxUI(coordinates.start.client, coordinates.start.client);
+    },
+    overlayMouseMove(event) {
+        if (!isMouseDown) {
+            return;
+        }
+
+        coordinates.end.page.x = event.pageX;
+        coordinates.end.page.y = event.pageY;
+        coordinates.end.client.x = event.clientX;
+        coordinates.end.client.y = event.clientY;
+
+        renderSelectionBoxUI(coordinates.start.client, coordinates.end.client);
+    },
+    overlayMouseUp(event) {
+        isMouseDown = false;
+
+        coordinates.end.page.x = event.pageX;
+        coordinates.end.page.y = event.pageY;
+        coordinates.end.client.x = event.clientX;
+        coordinates.end.client.y = event.clientY;
+
+        renderSelectionBoxUI(coordinates.start.client, coordinates.end.client);
+    },
+};
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === 'clicked_browser_action') {
@@ -9,7 +68,11 @@ chrome.runtime.onMessage.addListener(
             } else if (displayProp === 'none') {
                 overlay.show();
             } else {
-                createExtensionOverlay();
+                const newOverlay = createExtensionOverlay();
+
+                newOverlay.on('mousedown', eventHandlers.overlayMouseDown);
+                newOverlay.on('mouseup', eventHandlers.overlayMouseUp);
+                newOverlay.on('mousemove', eventHandlers.overlayMouseMove);
             }
         }
     }
@@ -36,4 +99,32 @@ function createExtensionOverlay() {
     `;
 
     overlay.html(topMenuHtmlString);
+
+    return overlay;
+}
+
+function renderSelectionBoxUI(coor1, coor2) {
+    const overlay = $('#pixel-perfect-overlay');
+    const boxWidth = Math.abs(coor1.x - coor2.x);
+    const boxHeight = Math.abs(coor1.y - coor2.y);
+    let boxLeft = coor1.x;
+    let boxTop = coor1.y;
+
+    // user drags towards the left
+    if (coor1.x > coor2.x) {
+        boxLeft = coor2.x;
+    }
+
+    // user drags upwards
+    if (coor1.y > coor2.y) {
+        boxTop = coor2.y;
+    }
+
+    $('.pp-selection-box').remove();
+    overlay.append(`
+        <div
+            class="pp-selection-box"
+            style="width: ${boxWidth}px; height: ${boxHeight}px; top: ${boxTop}px; left: ${boxLeft}px;"
+        />`
+    );
 }
