57b9d214013d92af19b13c10deafe016bc573e51
Ray Chan <football2000003@hotmail.com>
2019-04-07 20:25:53 +0800

    Add overlay element

--- js/content.js
+++ js/content.js
@@ -1,7 +1,39 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === 'clicked_browser_action') {
-            console.log('hello world');
+            const overlay = $('#pixel-perfect-overlay');
+            const displayProp = overlay.css('display');
+
+            if (displayProp === 'block') {
+                overlay.hide();
+            } else if (displayProp === 'none') {
+                overlay.show();
+            } else {
+                createExtensionOverlay();
+            }
         }
     }
 );
+
+function createExtensionOverlay() {
+    $('body').append('<div id="pixel-perfect-overlay"></div>');
+
+    const overlay = $('#pixel-perfect-overlay');
+    const topMenuHtmlString = `
+        <div class="pp-top-menu">
+            <div class="pp-top-menu__control-button-container">
+                <button>Top</button>
+                <button>Bottom</button>
+                <button>Left</button>
+                <button>Right</button>
+            </div>
+            <div class="pp-top-menu__pixel-display-container">
+                <input type="text" value="0px" />
+                <span>x</span>
+                <input type="text" value="0px" />
+            </div>
+        </div>
+    `;
+
+    overlay.html(topMenuHtmlString);
+}
